e5061fa62beae2bef76e720829c302b08643df52
CodeZage <hectorthoegersen@gmail.com>
2022-11-20 22:12:27 +0100

    Created portal with render textures.

--- Assets/Scripts/Portal/PortalWithRenderTexture.cs
+++ Assets/Scripts/Portal/PortalWithRenderTexture.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 namespace Portal
 {
-    public class Portal : MonoBehaviour
+    /// <summary>
+    /// Portal with a camera that renders the the image to a render-texture.
+    /// </summary>
+    public class PortalWithRenderTexture : MonoBehaviour
     {
         public Camera mainCamera;
 
@@ -49,5 +52,26 @@ namespace Portal
                 portalCamera.projectionMatrix = mainCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
             }
         }
+    }
+
+    public static class MathUtil
+    {
+        public static Quaternion QuaternionFromMatrix(Matrix4x4 m)
+        {
+            return Quaternion.LookRotation(m.GetColumn(2), m.GetColumn(1));
+        }
+
+        public static Vector4 PosToV4(Vector3 v)
+        {
+            return new Vector4(v.x, v.y, v.z, 1.0f);
+        }
+
+        public static Vector3 ToV3(Vector4 v)
+        {
+            return new Vector3(v.x, v.y, v.z);
+        }
+
+        public static Vector3 ZeroV3 = new Vector3(0.0f, 0.0f, 0.0f);
+        public static Vector3 OneV3 = new Vector3(1.0f, 1.0f, 1.0f);
     }
 }
